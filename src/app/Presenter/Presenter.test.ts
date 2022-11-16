0156bad4a8730dea5aa53a29296722891865a30d
Riemelt <n.lychenkov@gmail.com>
2022-11-16 18:25:30 +0700

    Add Presenter tests

---
+++ src/app/Presenter/Presenter.test.ts
@@ -0,0 +1,223 @@
+import EventManager from "../EventManager/EventManager";
+import { SliderEvent } from "../EventManager/types";
+import Model from "../Model/Model";
+import { JustSliderOptions } from "../types";
+import View from "../View/View";
+import Presenter from "./Presenter";
+
+describe("Presenter", () => {
+  let presenter:    Presenter;
+  let eventManager: EventManager;
+  let model:        Model;
+  let view:         View;
+
+  const onUpdate = jest.fn(() => undefined);
+  
+  const options: JustSliderOptions = {
+    from:        0,
+    to:          100,
+    min:         0,
+    max:         100,
+    step:        10,
+    orientation: "horizontal",
+    direction:   "forward",
+    range:       false,
+    tooltips:    false,
+    progressBar: false,
+    onUpdate,
+  }
+
+  function buildPresenter() {
+    eventManager = new EventManager();
+    model        = new Model(eventManager);
+    view         = new View(eventManager);
+    presenter    = new Presenter(view, model, eventManager);
+  }
+
+  beforeEach(() => {
+    buildPresenter();
+    jest.restoreAllMocks();
+  });
+
+  test("Init model", () => {
+    const mockedModelInit = jest.spyOn(model, "init");
+
+    presenter.init(options);
+
+    expect(mockedModelInit).toBeCalled();
+  });
+
+  test("Init view", () => {
+    const mockedViewInit           = jest.spyOn(view, "init");
+    const mockedViewInitComponents = jest.spyOn(view, "initComponents");
+
+    presenter.init(options);
+
+    expect(mockedViewInit).toBeCalled();
+    expect(mockedViewInitComponents).toBeCalled();
+  });
+
+  test("Creates view handlers", () => {
+    const mockedViewHandleHandler      = jest.spyOn(view, "addCreateHandleHandlers");
+    const mockedViewSliderClickHandler = jest.spyOn(view, "addCreateSliderClickHandler");
+
+    presenter.init(options);
+
+    expect(mockedViewHandleHandler).toBeCalled();
+    expect(mockedViewSliderClickHandler).toBeCalled();
+
+    const mockedUpdate = jest.spyOn(model, "updateHandle");
+
+    const handleHandler = mockedViewHandleHandler.mock.calls[0][0];
+    const sliderClickHandler = mockedViewSliderClickHandler.mock.calls[0][0];
+
+    handleHandler(50, "from");
+    sliderClickHandler(50, "from");
+
+    expect(mockedUpdate).toBeCalledTimes(2);
+  });
+
+  test("Registers events", () => {
+    const mockedRegister = jest.spyOn(eventManager, "registerEvent");
+    const events: Array<SliderEvent> = [
+      "HandleFromMove",
+      "HandleToMove",
+      "SliderUpdate",
+      "OrientationUpdate",
+      "TooltipsUpdate",
+      "ProgressBarUpdate",
+      "SliderClickDisable",
+      "SliderClickEnable",
+    ];
+
+    presenter.init(options);
+    
+    events.forEach(event => {
+      expect(mockedRegister).toBeCalledWith(event);
+    });
+  });
+
+  test("Dispatches events on initialization", () => {
+    const mockedDispatch = jest.spyOn(eventManager, "dispatchEvent");
+    const events: Array<SliderEvent> = [
+      "HandleFromMove",
+      "HandleToMove",
+      "ProgressBarUpdate",
+      "OrientationUpdate",
+      "TooltipsUpdate",
+      "SliderClickEnable",
+      "SliderUpdate",
+    ];
+
+    presenter.init(options);
+
+    events.forEach(event => {
+      expect(mockedDispatch).toBeCalledWith(event);
+    });
+  });
+
+  describe("Adds event listeners", () => {
+    test("HandleFromMove", () => {
+      presenter.init(options);
+
+      const mockedUpdate = jest.spyOn(view, "updateHandleFrom");
+      eventManager.dispatchEvent("HandleFromMove");
+
+      expect(mockedUpdate).toBeCalledTimes(1);
+    });
+
+    test("HandleToMove", () => {
+      presenter.init(options);
+
+      const mockedUpdate = jest.spyOn(view, "updateHandleTo");
+      eventManager.dispatchEvent("HandleToMove");
+
+      expect(mockedUpdate).toBeCalledTimes(1);
+    });
+
+    test("ProgressBarUpdate", () => {
+      presenter.init(options);
+
+      const mockedUpdate = jest.spyOn(view, "updateProgressBar");
+      eventManager.dispatchEvent("ProgressBarUpdate");
+
+      expect(mockedUpdate).toBeCalledTimes(1);
+    });
+
+    test("OrientationUpdate", () => {
+      presenter.init(options);
+
+      const mockedUpdate = jest.spyOn(view, "setOrientation");
+      eventManager.dispatchEvent("OrientationUpdate");
+
+      expect(mockedUpdate).toBeCalledTimes(1);
+    });
+
+    test("TooltipsUpdate", () => {
+      presenter.init(options);
+
+      const mockedUpdate = jest.spyOn(view, "updateTooltips");
+      eventManager.dispatchEvent("TooltipsUpdate");
+
+      expect(mockedUpdate).toBeCalledTimes(1);
+    });
+
+    test("SliderClickEnable", () => {
+      presenter.init(options);
+
+      const mockedHandlerSwitch = jest.spyOn(view, "setSliderClickHandler");
+      eventManager.dispatchEvent("SliderClickEnable");
+
+      expect(mockedHandlerSwitch).toBeCalledTimes(1);
+    });
+
+    test("SliderClickDisable", () => {
+      presenter.init(options);
+
+      const mockedHandlerSwitch = jest.spyOn(view, "removeSliderClickHandler");
+      eventManager.dispatchEvent("SliderClickDisable");
+
+      expect(mockedHandlerSwitch).toBeCalledTimes(1);
+    });
+
+    test("SliderUpdate", () => {
+      presenter.init(options);
+
+      onUpdate.mockReset();
+      eventManager.dispatchEvent("SliderUpdate");
+
+      expect(onUpdate).toBeCalledTimes(1);
+    });
+  });
+
+  test("Updates a handle", () => {
+    const mockedUpdate = jest.spyOn(model, "updateHandle");
+
+    presenter.init(options);
+    presenter.updateHandle("from", 50);
+
+    expect(mockedUpdate).toBeCalledWith(50, "from");
+  });
+
+  test("Updates options", () => {
+    const mockedUpdate = jest.spyOn(model, "updateOptions");
+    const newOptions = {
+      min: 1000,
+      max: 2000,
+    };
+
+    presenter.init(options);
+    presenter.updateOptions(newOptions);
+
+    expect(mockedUpdate).toBeCalledWith(newOptions);
+  });
+
+  test("Returns slider's html node", () => {
+    const mockedGetHtml = jest.spyOn(view, "getHtml");
+
+    presenter.init(options);
+    const $html = presenter.$getSlider();
+
+    expect($html).toEqual(mockedGetHtml.mock.results[0].value);
+  });
+});
